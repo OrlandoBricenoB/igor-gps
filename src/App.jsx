5c87e6f86b7df7067ee082d6abc5feaca6dc59ca
orlandobriceno <orlando.briceno.blanco@gmail.com>
2022-06-09 07:07:45 -0400

    feat: Header, Ubicación LatLng y UTM + Precisión

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
+import Header from './components/Header'
 import './css/App.css'
-import { getCurrentPosition, getUTMPosition } from './helpers/locationHelper'
+import { displayLocation, getCurrentPosition, getUTMPosition } from './helpers/locationHelper'
 
 const App = () => {
   const [currentLocation, setCurrentLocation] = useState(null)
@@ -16,35 +17,68 @@ const App = () => {
   // * Botón de obtener ubicación.
   const handleClickGetCurrentPosition = () => {
     getCurrentPosition(position => {
-      console.log(position)
       setCurrentLocation(position)
     })
   }
 
   return (
-    <div className="App">
-      <p>Me encanta React puro.</p>
-      <p>Made with ♥ by Jo-Sword</p>
-      <button onClick={handleClickGetCurrentPosition}>Get Location</button>
-      <p>
-        Locación:<br/>
-        Latitud: {currentLocation?.latitude}<br/>
-        Longitud: {currentLocation?.longitude}<br/>
-        Precisión: {currentLocation?.accuracy} metros.
-      </p>
-      <p>
-        UTM:
-        <br/>
-        Zona: {currentUTM?.ZoneNumber}
-        <br/>
-        Cuadrante: {currentUTM?.ZoneLetter}
-        <br/>
-        E {currentUTM?.Easting}
-        <br/>
-        N {currentUTM?.Northing}
-        <br/>
-      </p>
-    </div>
+    <>
+      <Header handleGetPosition={handleClickGetCurrentPosition} />
+      <main className='App'>
+        {
+          currentLocation
+            ? (
+              <section className='location'>
+                <h2 className="location__title">Ubicación</h2>
+                <h3 className="h3">LAT</h3>
+                <p>
+                  <span className='location__coord--displayed'>{displayLocation(currentLocation?.latitude, 'lat')}</span>
+                  <br/>
+                  {currentLocation?.latitude}<br/>
+                </p>
+                <h3 className="h3">LON</h3>
+                <p>
+                  <span className='location__coord--displayed'>{displayLocation(currentLocation?.longitude, 'lon')}</span>
+                  <br/>
+                  {currentLocation?.longitude}<br/>
+                </p>
+
+                <h3 className="h3">UTM</h3>
+                <p>
+                  <b>Zona:</b> {currentUTM?.ZoneNumber} <b>Cuadrante:</b> {currentUTM?.ZoneLetter}
+                  <br/>
+                  <b>E</b> {currentUTM?.Easting} <b>N</b> {currentUTM?.Northing}
+                  <br/>
+                </p>
+              </section>
+              )
+            : (
+              <section className="location">
+                <h2 className='location__title'>Ubicación</h2>
+                <p className='location__help-text'>
+                  Presiona el botón Recargar en la parte superior derecha de la aplicación.
+                </p>
+              </section>
+              )
+        }
+        {
+          currentLocation
+            ? (
+              <section className='location__accuracy'>
+                <h3 className='h3'>Precisión</h3>
+                <p className='location__accuracy-value'>
+                  {parseInt(currentLocation?.accuracy)} metros.
+                </p>
+              </section>
+              )
+            : (
+              <section className='location__accuracy'>
+                <h3 className='h3'>Para el abuelo Igor</h3>
+              </section>
+              )
+        }
+      </main>
+    </>
   )
 }
 
